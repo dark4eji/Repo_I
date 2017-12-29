744ce08661453ec93b1d80070384f4cc014dd43b
dark4eji <dark4eji@gmail.com>
2017-12-29 22:04:08 +0600

    Modify DUGP.rb

--- ascd/Dispatcher Quick User Guide/Dispatcher User Guide Publisher.rb
+++ ascd/Dispatcher Quick User Guide/Dispatcher User Guide Publisher.rb
@@ -9,38 +9,78 @@ else
 end
 
 # Задаём атрибуты использования кастомного шаблона и шрифтов + путь. Путь относительный.
-fonts = ' -a pdf-fontsdir="..\System_files\sptt_fonts"'
-template = ' -a pdf-style="..\System_files\sptt_pdf_template.yml"'
+fonts_path = ' -a pdf-fontsdir="..\System_files\sptt_fonts"'
+pdf_template_path = ' -a pdf-style="..\System_files\sptt_pdf_template.yml"'
+css_template_path = ' -a stylesheet="..\..\System_files\chm_style.css"'
 
-# Объявляем переменную для цикла приёма ответа.
-language = 0
+# Объявление переменных циклов
+language = "none"
+doctype = "none"
 
 # Цикл приёма ответа. Соответственно, если введённый ответ не совпадает
-# с нужным, то будет выводится сообщение после else и петля повторяется.
+# с нужным, то будет выводиться сообщение после else и петля повторяется.
 loop do
 # Вывод вопроса
-  print("Enter the desired language[eng/rus/pt]: ")
+  print("Enter the desired language[en/ru/pt]: ")
+
 # Приём ответа
-  input = gets.chomp
-  # Проверка введённого ответа и назначение языка
-  if input == "eng"
-    language = 'EN'
-	break
-  elsif input == "rus"
-    language = 'RU'
-	break
-  else puts 'Try another' # Значение false возвращающее цикл в начало
-  end  
+  language = gets.chomp
+  language = language.upcase
+
+  # Проверка введённого ответа и назначение конечного значения переменной языка
+  if language == "EN" || language == "RU" || language == "PT"
+    break
+  else
+    puts 'Please, try again' # Значение false возвращающее цикл в начало
+  end
 end
 
-# Собираем переменную конченой папки
+loop do
+
+  print("Enter the doc type[pdf/html]: ")
+  doctype = gets.chomp
+  doctype = doctype.upcase
+
+  if doctype == "PDF"
+    doctype = "asciidoctor-pdf"
+	css_template_path = ''
+    break
+
+  elsif doctype == "HTML"
+    doctype = "asciidoctor"
+	fonts_path = ''
+	pdf_template_path = ''
+
+    loop do
+        print "Use the default html theme?[yes/no]: "
+        answer = gets.chomp
+        answer = answer.upcase
+
+        if answer == "YES"
+          css_template_path = ''
+          break
+        elsif answer == "NO"
+          break
+        else 'Please, try again'
+        end
+     end
+    break
+
+  else
+    puts 'Please, try again'
+  end
+end
+
+# Собираем переменную папки выгрузки
 output_folder = "#{driveind}PUBLISHED\\Asc_#{language}"
 
-# Собираем переменную до файла-ток-проекта
+# Собираем переменную пути к файлу ток/проекта
 mainpath = "'#{language}\\Dispatcher Quick User Guide.adoc'"
+add_attrs = "#{fonts_path} #{pdf_template_path} #{css_template_path}"
 
 # Обращаемся к ОС окружению и публикуем файл
-system "asciidoctor-pdf -D #{output_folder} #{mainpath}"
+#system "asciidoctor-pdf -D #{output_folder} #{add_attrs} #{mainpath}"
+system "#{doctype} -D #{output_folder} #{add_attrs} #{mainpath}"
 
 # Пауза для просмотра лога
 sleep
